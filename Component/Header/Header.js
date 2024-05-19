e8ac08998f7bb4c677914258da2f56a726f0d932
Farzane Nazmabadi <1379farzanenazmabadi@gmail.com>
2024-05-19 16:15:28 +0430

    use lifecycle for header component and adding mobile nav menu

--- Component/Header/Header.js
+++ Component/Header/Header.js
@@ -13,6 +13,13 @@ template.innerHTML = `
         </ul>
         <div class="mobile-menu">
             <div class="hamburger-menu"><span class="hamburger-menu-line"></span></div>
+            <ul class="nav-menu-mobile ">
+                <li class="menu-item-mobile"><a href="#">Home</a></li>
+                <li class="menu-item-mobile"><a href="#">Product</a></li>
+                <li class="menu-item-mobile"><a href="#">Shop</a></li>
+                <li class="menu-item-mobile"><a href="#">Contact Us</a></li>
+                <li class="menu-item-mobile"><a href="#">Cart</a></li>
+            </ul>
         </div>
         <div class="logo">
          <img src="starbucks-logo1 (2).png" alt="starbox logo" title="starbucks logo">
@@ -26,6 +33,23 @@ class CustomHeader extends HTMLElement {
             mode: "open"
         })
         this.shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+    connectedCallback(){
+        let hamburgerMenu=this.shadowRoot.querySelector(".hamburger-menu")
+        let mobileMenu=this.shadowRoot.querySelector(".nav-menu-mobile")
+        console.log(hamburgerMenu);
+        hamburgerMenu.addEventListener("click",()=>{
+            this.openMobileMenu(this,hamburgerMenu,mobileMenu)
+        })
+    }
+    openMobileMenu(el,hamburgerMenu,mobileMenu){
+        console.log(el);
+        hamburgerMenu.classList.toggle("nav-menu-mobile--open")
+        if(hamburgerMenu.classList.contains("nav-menu-mobile--open")){
+            mobileMenu.style.right="0"
+        }else{
+            mobileMenu.style.right="-50vw"
+        }
     }
 }
 export {
