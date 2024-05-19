2225b2962ec5b8c896dbe84b50c3e6a5e0bd559f
Farzane Nazmabadi <1379farzanenazmabadi@gmail.com>
2024-05-19 17:40:24 +0430

    dinamic option component

--- Component/Options/Options.js
+++ Component/Options/Options.js
@@ -1,151 +1,42 @@
-const option1=document.createElement("template")
-const option2=document.createElement("template")
-const option3=document.createElement("template")
-const option4=document.createElement("template")
-const option5=document.createElement("template")
-const option6=document.createElement("template")
+const option=document.createElement("template")
 
-option1.innerHTML=
+option.innerHTML=
 `
     <link rel="stylesheet" href="Component/Options/options.css">
     <div class="options option1">
         <div class="options__image">
-            <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-92090.jpg" alt="">
+            <img src="" alt="" class="options__image-img">
         </div>
         <div class="options__caption">
-            <h3 class="options__title">50% off a drink when you get the app</h3>
-            <p class="options__text">
-                Get 50% off a drink when you download the app and join Starbucks Rewards by the end of May. Just download the app to get started.*
-            </p>
-            <button class="options__button">Sign Up Here</button>
+            <h3 class="options__title"></h3>
+            <slot name="option-text"></slot>
+            <button class="options__button"></button>
         </div>
     </div>
 `
 
-option2.innerHTML=
-`
-    <link rel="stylesheet" href="Component/Options/options.css">
-    <div class="options option2">
-        <div class="options__image">
-            <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-91829.jpg" alt="">
-        </div>
-        <div class="options__caption option2">
-            <h3 class="options__title">Summer at first sip</h3>
-            <p class="options__text">
-                Say hello to fun new flavors and classic favorites.
-            </p>
-            <button class="options__button">Order Now</button>
-        </div>
-    </div>
-`
 
-option3.innerHTML=
-`
-    <link rel="stylesheet" href="Component/Options/options.css">
-    <div class="options option3">
-        <div class="options__image">
-            <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-91966.jpg" alt="">
-        </div>
-        <div class="options__caption">
-            <h3 class="options__title">The best deals are in the app</h3>
-            <p class="options__text">
-                Tap into new offers all summer long. In May, enjoy deal drops on Mondays, 50% off your drink from 12–6 PM on Fridays and more. **
-            </p>
-            <button class="options__button">Get The App</button>
-        </div>
-    </div>
-`
+class CustomeOPtion extends HTMLElement{
+    constructor(){
+        super()
+        this.attachShadow({mode:"open"})
+        this.shadowRoot.appendChild(option.content.cloneNode(true))
+    }
+    connectedCallback(){
+        let optionTitle=this.shadowRoot.querySelector(".options__title")
+        optionTitle.innerHTML=this.getAttribute("option-title")
 
-option4.innerHTML=
-`
-    <link rel="stylesheet" href="Component/Options/options.css">
-    <div class="options option4">
-        <div class="options__image">
-            <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-91974.png" alt="">
-        </div>
-        <div class="options__caption">
-            <h3 class="options__title">When you gift, we give</h3>
-            <p class="options__text">
-                For every Military Appreciation eGift purchased in May, we’ll donate $5 to support the military community.
-            </p>
-            <button class="options__button">Send an eGift</button>
-        </div>
-    </div>
-`
+        let optionImage=this.shadowRoot.querySelector(".options__image-img")
+        optionImage.setAttribute("src",this.getAttribute("option-image"))
 
-option5.innerHTML=
-`
-    <link rel="stylesheet" href="Component/Options/options.css">
-    <div class="options option5">
-        <div class="options__image">
-            <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-91976.jpg" alt="">
-        </div>
-        <div class="options__caption">
-            <h3 class="options__title">Dads, grads & Starbucks Cards</h3>
-            <p class="options__text">
-                 Got a Starbucks Card? Start earning free drinks & food twice as fast when you join and use your Starbucks Cards in the app.***
-            </p>
-            <button class="options__button">Get The App</button>
-        </div>
-    </div>
-`
-option6.innerHTML=
-`
-    <link rel="stylesheet" href="Component/Options/options.css">
-    <div class="options option6">
-        <div class="options__image">
-            <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-91833.jpg" alt="">
-        </div>
-        <div class="options__caption">
-            <h3 class="options__title">“I never thought I would go to university. Starbucks has made this dream a reality.”</h3>
-            <p class="options__text">
-                  – Melinda, Starbucks partner (employee) and Starbucks College Achievement Plan graduate​
-            </p>
-            <button class="options__button">Learn More</button>
-        </div>
-    </div>
-`
+        let optionBtn=this.shadowRoot.querySelector(".options__button")
+        optionBtn.innerHTML=this.getAttribute("btn-text")
 
-class CustomeOPtion1 extends HTMLElement{
-    constructor(){
-        super()
-        this.attachShadow({mode:"open"})
-        this.shadowRoot.appendChild(option1.content.cloneNode(true))
+        let optionNumber=this.shadowRoot.querySelector(".options")
+        optionNumber.classList.add(this.getAttribute("option-number"))
+    }
+    static observedAttribute(){
+        return["option-title","option-image","btn-text","option-number"]
     }
 }
-class CustomeOPtion2 extends HTMLElement{
-    constructor(){
-        super()
-        this.attachShadow({mode:"open"})
-        this.shadowRoot.appendChild(option2.content.cloneNode(true))
-    }
-}
-class CustomeOPtion3 extends HTMLElement{
-    constructor(){
-        super()
-        this.attachShadow({mode:"open"})
-        this.shadowRoot.appendChild(option3.content.cloneNode(true))
-    }
-}
-class CustomeOPtion4 extends HTMLElement{
-    constructor(){
-        super()
-        this.attachShadow({mode:"open"})
-        this.shadowRoot.appendChild(option4.content.cloneNode(true))
-    }
-}
-class CustomeOPtion5 extends HTMLElement{
-    constructor(){
-        super()
-        this.attachShadow({mode:"open"})
-        this.shadowRoot.appendChild(option5.content.cloneNode(true))
-    }
-}
-class CustomeOPtion6 extends HTMLElement{
-    constructor(){
-        super()
-        this.attachShadow({mode:"open"})
-        this.shadowRoot.appendChild(option6.content.cloneNode(true))
-    }
-}
-export {CustomeOPtion1,CustomeOPtion2,CustomeOPtion3,CustomeOPtion4,CustomeOPtion5,CustomeOPtion6}
+export {CustomeOPtion}
--- js/app.js
+++ js/app.js
@@ -1,18 +1,8 @@
 import { CustomHeader } from "../Component/Header/Header.js"
 import { CustomFooter } from "../Component/Footer/Footer.js"
-import { CustomeOPtion1 } from "../Component/Options/Options.js"
-import { CustomeOPtion2 } from "../Component/Options/Options.js"
-import { CustomeOPtion3 } from "../Component/Options/Options.js"
-import { CustomeOPtion4 } from "../Component/Options/Options.js"
-import { CustomeOPtion5 } from "../Component/Options/Options.js"
-import { CustomeOPtion6 } from "../Component/Options/Options.js"
+import { CustomeOPtion } from "../Component/Options/Options.js"
 
 
 window.customElements.define("custome-header",CustomHeader)
 window.customElements.define("custom-footer",CustomFooter)
-window.customElements.define("custom-option1",CustomeOPtion1)
-window.customElements.define("custom-option2",CustomeOPtion2)
-window.customElements.define("custom-option3",CustomeOPtion3)
-window.customElements.define("custom-option4",CustomeOPtion4)
-window.customElements.define("custom-option5",CustomeOPtion5)
-window.customElements.define("custom-option6",CustomeOPtion6)
+window.customElements.define("custom-option",CustomeOPtion)
